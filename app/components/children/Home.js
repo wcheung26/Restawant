19a7a474d152d443b12ca084c08a2367e4db3715
Jonathan Wong <jdwong2@yahoo.com>
2017-08-25 20:15:18 -0700

    Home page design

--- app/components/children/Home.js
+++ app/components/children/Home.js
@@ -9,11 +9,25 @@ class Home extends Component {
   render() {
     return (
       <div className="col-md-12 text-center">
+        <div className="ribbon text-center">
+          <div className="col-md-6 icon-div">
+            <Link to="/restaurant/signup"><i className="home-icon fa fa-cutlery" aria-hidden="true"></i></Link>
+            <h4><strong>RESTAURANTS</strong></h4>
+            <p>Grow Your Business</p>
+          </div>
+          <div className="col-md-6 icon-div">
+            <Link to="/influencer/signup"><i className="home-icon fa fa-money" aria-hidden="true"></i></Link>
+            <h4><strong>INFLUENCERS</strong></h4>
+            <p>Earn Some Cash</p>
+          </div>
+        </div>
         <h3>Are you a restaurant looking for more business? Sign up with us today!</h3>
         <Link to="/restaurant/signup"><button type="button" className="btn btn-default">Join</button></Link>
         <br/>
         <h3>Looking to earn extra cash? Sign up with us today!</h3>
         <Link to="/influencer/signup"><button type="button" className="btn btn-default">Join</button></Link>
+
+
       </div>
     );
   }
