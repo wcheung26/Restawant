0e665250a92ece509c044a416c6a00e3881fda5b
Jonathan Wong <jdwong2@yahoo.com>
2017-08-28 16:55:10 -0700

    Fix modal

--- app/components/children/OtherView.js
+++ app/components/children/OtherView.js
@@ -5,13 +5,10 @@ import { Modal, Button } from 'react-bootstrap';
 import Navbar from "../children/Navbar";
 import RestaurantLogin from "../children/Restaurant/RestaurantLogin";
 import RestaurantSignup from "../children/Restaurant/RestaurantSignup";
-import RestaurantDashboard from "../children/Restaurant/RestaurantDashboard";
 import InfluencerLogin from "../children/Influencer/InfluencerLogin";
 import InfluencerSignup from "../children/Influencer/InfluencerSignup";
-import InfluencerDashboard from "../children/Influencer/InfluencerDashboard";
 import AdminLogin from "../children/Admin/AdminLogin";
 import AdminSignup from "../children/Admin/AdminSignup";
-import AdminDashboard from "../children/Admin/AdminDashboard";
 import Home from "../children/Home";
 import Logout from "../children/Logout";
 
@@ -20,29 +17,12 @@ import helpers from "../utils/helpers";
 class OtherView extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      showModal: false
-    }
-    
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
-
-
-  closeModal() {
-    this.setState({ showModal: false });
-  }
-
-  openModal() {
-    console.log("Test");
-    this.setState({ showModal: true });
   }
 
   render() {
     return (
       <div>
-        <Navbar userAuth={this.props.userAuth} openModal={this.state.openModal} />
+        <Navbar userAuth={this.props.userAuth} openModal={this.props.openModal} />
         <div className="container-fluid main">
           <div className="row">
             <Switch>
@@ -68,13 +48,13 @@ class OtherView extends Component {
               )}
               <Route path="/admin/signup" component={AdminSignup} />
               { this.props.adminAuth && this.props.userAuth ? (
-                  <Route path="/admin" component={AdminDashboard} />
-                ) : (
                   null
+                ) : (
+                  <Route path="/admin" component={AdminLogin} />
               )}
               <Route path="/" component={Home} />
             </Switch>
-            <Modal show={this.state.showModal} onHide={this.closeModal}>
+            <Modal show={this.props.showModal} onHide={this.props.closeModal}>
               <Modal.Header closeButton>
                 <Modal.Title>About Restawant</Modal.Title>
               </Modal.Header>
@@ -82,7 +62,7 @@ class OtherView extends Component {
                 <h4>A platform assisting restaurants to manage and grow their social media marketing.</h4>
               </Modal.Body>
               <Modal.Footer>
-                <Button onClick={this.closeModal}>Close</Button>
+                <Button onClick={this.props.closeModal}>Close</Button>
               </Modal.Footer>
             </Modal>
           </div>
